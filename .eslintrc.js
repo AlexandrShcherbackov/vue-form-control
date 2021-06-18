5c3fccb4fe156f6306316b7e5a8e208db69b8178
AlexandrShcherbackov <jsprog2017@yandex.ru>
2021-06-18 12:35:03 +0300

    add tailwind

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': ['error', {
+      code: 120,
+      ignorePattern: 'import(.*)|background-image:\\s(.*)',
+    }],
+    'no-underscore-dangle': ['off'],
+    'linebreak-style': ['off'],
+    'padded-blocks': ['off'],
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
